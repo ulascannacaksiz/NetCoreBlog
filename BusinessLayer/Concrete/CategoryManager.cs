800230096f38c10e664ba9e5db84c25ed96417cc
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-02-10 14:18:25 +0300

    .net core blog 10 subat

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //CategoryRepository categoryRepository = new CategoryRepository();
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public void CategoryAdd(Category category)
+        {
+            //if(category.CategoryName!="" && category.CategoryDescription!="" && category.CategoryName.Length>=5 && category.CategoryStatus == true)
+            //{
+            //    categoryRepository.AddCategory(category);
+            //}
+            //else
+            //{
+            //    //Hata mesajı 
+            //}
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            if(category.CategoryID != 0)
+            {
+                repo.Delete(category);
+            }
+            
+
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=CoreBlogDB;Integrated Security=True;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
