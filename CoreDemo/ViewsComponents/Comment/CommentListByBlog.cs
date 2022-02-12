615438622696377ce05e148ea5916c4e24b6da6a
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-02-12 13:24:42 +0300

    Yorum Listeleme işleri ve blog devamını oku yapıldı

---
+++ CoreDemo/ViewsComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewsComponents.Comment
+{
+    public class CommentListByBlog: ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(5);
+            return View(values);
+        }
+    }
+}
