c841d2bbd1568cc78a1455053bfb19e8f6c9d329
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-03-01 13:32:42 +0300

    pagination ve excel export işlemleri yapıldı

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -8,10 +12,50 @@ namespace CoreDemo.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = cm.GetList();
+            var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = cm.TGetById(id);
+            value.CategoryStatus = false;
+            cm.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        public IActionResult CategoryActivate(int id)
+        {
+            var value = cm.TGetById(id);
+            value.CategoryStatus = true;
+            cm.TUpdate(value);
+            return RedirectToAction("Index");
         }
     }
 }
