846b89c6f016af5f19110d7b4236e0ee5bb16589
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-03-04 10:04:34 +0300

    İstatistik,chart,widget,ajax yapıldı

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            string api = "Your api key";
+            string connection = "http://api.weatherapi.com/v1/current.xml?key="+api+"&q=İstanbul";
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temp_c").First().Value;
+            return View();
+        }
+    }
+}
