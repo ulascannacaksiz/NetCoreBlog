3000a90922e2fcf17eb52bd9431121bf8dd6a69f
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-03-04 11:02:14 +0300

    Api Demo yapıldı

---
+++ BlogApiDemo/DataAcessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAcessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=CoreBlogApiDB;Integrated Security=True;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
