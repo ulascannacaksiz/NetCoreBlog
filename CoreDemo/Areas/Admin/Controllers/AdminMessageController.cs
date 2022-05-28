4d82cc0ae782c35bdb48e29eec89097ae735fa44
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-05-28 20:26:43 +0300

    Admin mesaj kısımları yapıldı

---
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class AdminMessageController : Controller
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
+        public IActionResult Inbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult Sendbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetSendboxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult ComposeMail()
+        {
+            return View();
+        }
+    }
+}
