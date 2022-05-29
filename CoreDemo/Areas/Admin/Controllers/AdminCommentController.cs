ee10576aca581113d9734b8036f3036361745f81
Ulaş Can Nacaksız <ulascannacaksiz@hotmail.com>
2022-05-29 12:29:46 +0300

    Admin yorumlar ve  bloglar kısmı yapıldı

---
+++ CoreDemo/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCommentController : Controller
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetCommentWithBlog();
+            return View(values);
+        }
+    }
+}
--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
@@ -29,9 +31,24 @@ namespace CoreDemo.Areas.Admin.Controllers
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult ComposeMail()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMail(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);
+            return RedirectToAction("Sendbox");
         }
     }
 }
